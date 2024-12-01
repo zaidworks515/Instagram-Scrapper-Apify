ef7d9c99d160ba12794f8b7aee8c4a2ae52034bb
Zaid Ahmed <zaid_works515@outlook.com>
2024-12-01 19:07:33 +0500

    background task added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, BackgroundTasks
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from apify_client import ApifyClient
@@ -27,7 +27,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-executor = ThreadPoolExecutor(max_workers=20)
+# executor = ThreadPoolExecutor(max_workers=20)
 
 
 def data_scrapping(session_id, token, result_limit=None, hashtag1=None, hashtag2=None, hashtag3=None, min_followers=None):
@@ -70,7 +70,8 @@ def data_scrapping(session_id, token, result_limit=None, hashtag1=None, hashtag2
         print('profiles data extracted successfully.....')
         
         upload_result = cloudinary.uploader.upload(f"scrapped data/{session_id}.csv",
-                                           public_id=session_id, resource_type="raw")
+                                           public_id=session_id, resource_type="raw",
+                                           timeout=120000)
         
         if upload_result:
             cloudinary_url = upload_result['url']
@@ -120,7 +121,6 @@ def preprocessing_dataframe1(dataframe, session_id):
         raise
 
 
-
 def fetch_profiles(session_id, token, user_names, min_followers):
     print('executing fetch_profiles')
     try:
@@ -149,7 +149,10 @@ def fetch_profiles(session_id, token, user_names, min_followers):
 
 
 @app.post("/scrape_data")
-async def scrape_data(hashtag1: str, hashtag2: str = None, hashtag3: str = None, result_limit: int = 10, min_followers: int = None):
+async def scrape_data(hashtag1: str, hashtag2: str = None, 
+                      hashtag3: str = None, result_limit: int = 10, 
+                      min_followers: int = None, 
+                      background_tasks: BackgroundTasks = None):
 
     try:
         session_id = str(uuid.uuid4())
@@ -158,7 +161,10 @@ async def scrape_data(hashtag1: str, hashtag2: str = None, hashtag3: str = None,
         
         insert_data(session_id=session_id, file_path=file_path, creation_status=0)
 
-        executor.submit(data_scrapping, session_id, token, result_limit, hashtag1, hashtag2, hashtag3, min_followers)
+        # with ThreadPoolExecutor(max_workers=20) as executor:
+        #     executor.submit(data_scrapping, session_id, token, result_limit, hashtag1, hashtag2, hashtag3, min_followers)
+
+        background_tasks.add_task(data_scrapping, session_id, token, result_limit, hashtag1, hashtag2, hashtag3, min_followers)
 
         return JSONResponse(content={'message': response_message, 'session_id': session_id}, status_code=200)
 
@@ -171,20 +177,19 @@ async def scrape_data(hashtag1: str, hashtag2: str = None, hashtag3: str = None,
 async def get_data(session_id: str):
     try:
         start_time = time.time()
-        timeout = 10  
+        timeout = 10  # Timeout value in seconds
 
         while True:
             if time.time() - start_time > timeout:
                 raise HTTPException(status_code=408, detail="Timeout waiting for data to be ready")
 
-            await asyncio.sleep(2)
-            
-            status = check_status(session_id)
+            # Use asyncio to run the blocking task (check_status) in a separate thread via the executor
+            loop = asyncio.get_running_loop()
+            status = await loop.run_in_executor(None, check_status, session_id)
 
             if status['creation_status'] == 1:
                 try:
                     cloudinary_url = status['cloudinary_url']
-
                     return JSONResponse(content={'cloudinary_url': cloudinary_url}, status_code=200)
                 except FileNotFoundError:
                     logging.error(f"File for session {session_id} not found.")
@@ -193,6 +198,8 @@ async def get_data(session_id: str):
                     logging.error(f"Error reading file for session {session_id}: {str(e)}")
                     return JSONResponse(content={'cloudinary_url': 'error reading file'}, status_code=500)
 
+            await asyncio.sleep(2)  # Sleep before trying again
+
     except Exception as e:
         logging.error(f"Unexpected error occurred: {str(e)}")
         raise HTTPException(status_code=500, detail="An internal error occurred")
@@ -200,7 +207,8 @@ async def get_data(session_id: str):
 
 @app.on_event("shutdown")
 async def shutdown_event():
-    executor.shutdown(wait=True)
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        executor.shutdown(wait=True)
 
 
 if __name__ == "__main__":
